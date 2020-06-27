2552aa6c663461a6ed18ac8461647035d97a7e0b
Jacek Schae <jacek.schae@gmail.com>
2020-06-27 04:00:05 +0200

    08-start

---
+++ increments/07-heroku-deploy/project.clj
@@ -0,0 +1,20 @@
+(defproject cheffy "0.1.0-SNAPSHOT"
+  :description "Cheffy REST API"
+  :url "https://api.learnreitit.com"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring "1.8.1"]
+                 [integrant "0.8.0"]
+                 [environ "1.2.0"]
+                 [metosin/reitit "0.5.2"]
+                 [seancorfield/next.jdbc "1.0.462"]
+                 [org.postgresql/postgresql "42.2.14"]
+                 [clj-http "3.10.0"]
+                 [ovotech/ring-jwt "1.2.5"]]
+  :main ^:skip-aot cheffy.server
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}
+             :dev {:source-paths ["dev/src"]
+                   :resource-paths ["dev/resources"]
+                   :dependencies [[ring/ring-mock "0.4.0"]
+                                  [integrant/repl "0.3.1"]]}}
+  :uberjar-name "cheffy.jar")
---
+++ increments/08-start/src/cheffy/server.clj
@@ -0,0 +1,10 @@
+(ns cheffy.server
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(comment
+  (-main))
