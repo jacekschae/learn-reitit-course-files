51535b93c93b09c4a38a76e1e327d64f1af0e4f9
Jacek Schae <jacek.schae@gmail.com>
2020-10-19 15:01:17 +0200

    Fix for 11-integrant-reloaded

--- increments/11-integrant-reloaded/src/cheffy/server.clj
+++ increments/11-integrant-reloaded/src/cheffy/server.clj
@@ -1,37 +1,46 @@
 (ns cheffy.server
-  (:require [integrant.core :as ig]
+  (:require [reitit.ring :as ring]
             [ring.adapter.jetty :as jetty]
-            [cheffy.router :as router]
+            [integrant.core :as ig]
             [environ.core :refer [env]]))
 
-(defn app
-  [env]
-  (router/routes env))
+(def app
+  (ring/ring-handler
+    (ring/router
+      [["/"]
+       {:get {:handler (fn [req] {:staus 200}
+                         :body "Hello Reiti")}}])))
 
-(defmethod ig/prep-key :server/jetty [_ config]
+(defmethod ig/prep-key :server/jetty
+  [_ config]
   (merge config {:port (Integer/parseInt (env :port))}))
 
-(defmethod ig/init-key :server/jetty [_ {:keys [handler port]}]
-  (println (str "\nServer running on port " port "\n"))
-  (jetty/run-jetty handler {:port port :join? false}))
+(defmethod ig/init-key :server/jetty
+  [_ {:keys [handler port]}]
+  (println (str "\nServer running on port " port))
+  (jetty/run-jetty handler {:port port :join false}))
 
-(defmethod ig/init-key :cheffy/app [_ config]
+(defmethod ig/init-key :cheffy/app
+  [_ config]
   (println "\nStarted app")
   (app config))
 
-(defmethod ig/init-key :db/postgres [_ config]
-  (println (str "\nConfigured db"))
-  (merge config {:db (env :jdbc-database-url)})
-  (:db config))
+(defmethod ig/init-key :db/postgres
+  [_ config]
+  (println "\nConfigured db")
+  (:jdbc-url config))
 
-(defmethod ig/init-key :auth0/manage-user [_ {:keys [auth0]}]
-  (println "\nauht0/manage-user loaded\n")
-  auth0)
-
-(defmethod ig/halt-key! :server/jetty [_ jetty]
+(defmethod ig/halt-key! :server/jetty
+  [_ jetty]
   (.stop jetty))
 
 (defn -main
   [config-file]
   (let [config (-> config-file slurp ig/read-string)]
     (-> config ig/prep ig/init)))
+
+(comment
+  (start)
+  (app {:request-method :get
+        :uri "/"})
+  (-main))
