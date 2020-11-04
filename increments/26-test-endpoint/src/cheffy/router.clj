6eb9cf156b00dd3b96d393f035694c1df1c17a11
Jacek Schae <jacek.schae@gmail.com>
2020-11-04 15:11:46 +0100

    26-test-endpoint

---
+++ increments/26-test-endpoint/src/cheffy/router.clj
@@ -0,0 +1,40 @@
+(ns cheffy.router
+  (:require [reitit.ring :as ring]
+            [cheffy.recipe.routes :as recipe]
+            [reitit.swagger :as swagger]
+            [reitit.swagger-ui :as swagger-ui]
+            [muuntaja.core :as m]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [reitit.coercion.spec :as coercion-spec]
+            [reitit.ring.coercion :as coercion]
+            [reitit.ring.middleware.exception :as exception]))
+
+(def swagger-docs
+  ["/swagger.json"
+   {:get
+    {:no-doc  true
+     :swagger {:basePath "/"
+               :info     {:title       "Cheffy API Reference"
+                          :description "The Cheffy API is organized around REST. Returns JSON, Transit (msgpack, json), or EDN  encoded responses."
+                          :version     "1.0.0"}}
+     :handler (swagger/create-swagger-handler)}}])
+
+(def router-config
+  {:data {:coercion   coercion-spec/coercion
+          :muuntaja   m/instance
+          :middleware [swagger/swagger-feature
+                       muuntaja/format-middleware
+                       exception/exception-middleware
+                       coercion/coerce-request-middleware
+                       coercion/coerce-response-middleware]}})
+
+(defn routes
+  [env]
+  (ring/ring-handler
+    (ring/router
+      [swagger-docs
+       ["/v1"
+        (recipe/routes env)]]
+      router-config)
+    (ring/routes
+      (swagger-ui/create-swagger-ui-handler {:path "/"}))))
