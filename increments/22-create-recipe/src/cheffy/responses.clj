df84c5da80f00cbb3655b3a2badbb7728ab355fc
Jacek Schae <jacek.schae@gmail.com>
2020-11-09 12:24:41 +0100

    fix responses

--- increments/22-create-recipe/src/cheffy/responses.clj
+++ increments/22-create-recipe/src/cheffy/responses.clj
@@ -1,17 +1,31 @@
 (ns cheffy.responses
   (:require [spec-tools.data-spec :as ds]))
 
+(def step
+  {:step/step_id     string?
+   :step/sort        int?
+   :step/description string?
+   :step/recipe_id   string?})
+
+(def ingredient
+  {:ingredient/ingredient_id string?
+   :ingredient/sort          int?
+   :ingredient/name          string?
+   :ingredient/amount        int?
+   :ingredient/measure       string?
+   :ingredient/recipe_id     string?})
+
 (def recipe
-  {:recipe/public         boolean?
-   :recipe/favorite_count number?
-   :recipe/recipe_id      string?
-   :recipe/name           string?
-   :recipe/uid            string?
-   :recipe/prep_time      number?
-   :recipe/img            string?
-   :recipe/steps          any?
-   :recipe/ingredients    any?})
+  {:recipe/public               boolean?
+   :recipe/favorite_count       int?
+   :recipe/recipe_id            string?
+   :recipe/name                 string?
+   :recipe/uid                  string?
+   :recipe/prep_time            number?
+   :recipe/img                  string?
+   (ds/opt :recipe/steps)       [step]
+   (ds/opt :recipe/ingredients) [ingredient]})
 
 (def recipes
-  {:public [recipe]
+  {:public          [recipe]
    (ds/opt :drafts) [recipe]})
--- increments/26-test-endpoint/src/cheffy/responses.clj
+++ increments/26-test-endpoint/src/cheffy/responses.clj
@@ -3,16 +3,30 @@
 
 (def base-url "https://api.learnreitit.com")
 
+(def step
+  {:step/step-id     string?
+   :step/sort        int?
+   :step/description string?
+   :step/recipe-id   string?})
+
+(def ingredient
+  {:ingredient/ingredient-id string?
+   :ingredient/sort          int?
+   :ingredient/name          string?
+   :ingredient/amount        int?
+   :ingredient/measure       string?
+   :ingredient/recipe-id     string?})
+
 (def recipe
-  {:recipe/public         boolean?
-   :recipe/favorite_count number?
-   :recipe/recipe_id      string?
-   :recipe/name           string?
-   :recipe/uid            string?
-   :recipe/prep_time      number?
-   :recipe/img            string?
-   :recipe/steps          any?
-   :recipe/ingredients    any?})
+  {:recipe/public               boolean?
+   :recipe/favorite-count       int?
+   :recipe/recipe-id            string?
+   :recipe/name                 string?
+   :recipe/uid                  string?
+   :recipe/prep-time            number?
+   :recipe/img                  string?
+   (ds/opt :recipe/steps)       [step]
+   (ds/opt :recipe/ingredients) [ingredient]})
 
 (def recipes
   {:public          [recipe]
